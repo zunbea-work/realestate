210359b75d61124e25bf0fa48a8ef23b95590ae2
zunbea-work <k.og.workmail@gmail.com>
2026-03-15 09:04:29 +0900

    works　と　news　追加

--- js/main.js
+++ js/main.js
@@ -2,3 +2,25 @@ $("#js-hamburger").click(function () {
   $(".hamburger").toggleClass("is-active");
   $(".header-menu").toggleClass("is-open");
 });
+
+$(function(){
+  $(".modal-open").on("click", function(e) {
+    e.preventDefault();
+
+    const modalId =$(this).data("modal");
+    $("#modal-" + modalId).addClass("active");
+  });
+
+  $(".modal-container").on("click", function() {
+    $(this).removeClass("active");
+  });
+
+  $(".modal-close").on("click", function() {
+    $(".modal-container").removeClass("active");
+  });
+
+  $(".modal-body").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
+
