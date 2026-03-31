a6cb684a12f60185716e90847bffd75c6daef88a
zunbea-work <k.og.workmail@gmail.com>
2026-04-01 00:46:01 +0900

    スタイルの調整と新しいクラスを追加し、レスポンシブデザインを改善。モーダル機能を強化し、個人情報の取り扱いに関するセクションを追加。

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
+// ハンバーガー
 $("#js-hamburger").click(function () {
   $(".hamburger").toggleClass("is-active");
   $(".header-menu").toggleClass("is-open");
+  $("#js-overlay").toggleClass("is-open");
 });
+$("#js-overlay").click(function () {
+  $(".hamburger").removeClass("is-active");
+  $(".header-menu").removeClass("is-open");
+  $("#js-overlay").removeClass("is-open");
+});
+
+// モーダル
+let scrollY;
 
 $(function(){
   $(".modal-open").on("click", function(e) {
     e.preventDefault();
+
+    scrollY = $(window).scrollTop();
+
+    $("body").css({
+      position: "fixed",
+      top: -scrollY + "px",
+      width: "100%"
+    });
 
     const modalId =$(this).data("modal");
     $("#modal-" + modalId).addClass("active");
@@ -13,10 +31,26 @@ $(function(){
 
   $(".modal-container").on("click", function() {
     $(this).removeClass("active");
+
+    $("body").css({
+      position: "",
+      top: "",
+      width: ""
+    });
+
+    $(window).scrollTop(scrollY);
   });
 
   $(".modal-close").on("click", function() {
     $(".modal-container").removeClass("active");
+
+    $("body").css({
+      position: "",
+      top: "",
+      width: ""
+    });
+
+    $(window).scrollTop(scrollY);
   });
 
   $(".modal-body").on("click", function(e) {
@@ -24,6 +58,7 @@ $(function(){
   });
 });
 
+// FVスクロール
 var goTop = $("#fas-area");
 
 goTop.hide();
